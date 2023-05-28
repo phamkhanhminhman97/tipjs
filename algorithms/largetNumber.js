42a85e76d47a32678a1ee3c9dc2e51c6e55da8a4
phamkhanhminhman97 <phamkhanhminhman97@gmail.com>
2023-05-28 22:58:23 +0700

    largetNumber

---
+++ algorithms/largetNumber.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+
+
+let largestNumber = function (array) {
+  let x = {}
+  for (let index = 0; index < array.length; index++) {
+    x[array[index]] = String(array[index])
+
+  }
+  
+  x = Object.values(x).sort(
+    (p1, p2) => (p1 < p2) ? 1 : (p1 > p2) ? -1 : 0);
+  console.log(x);
+  let str = ''
+  for (let i = 0; i < x.length; i++) {
+    str += x[i]
+    
+  }
+  return str
+};
+
+let a = largestNumber([10, 2, 25]);
+console.log(a);
